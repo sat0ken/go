09ae7a24d8773c02ed88b5593af8568ce60b3796
sat0ken <15720506+sat0ken@users.noreply.github.com>
2020-05-23 19:57:59 +0900

    add files

---
+++ tour-of-go/map-literals-continued.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m = map[string]Vertex {
+	"Bell Labs": {40.68443, -7439967},
+	"Google":	 {37.42202, -122.08408},
+}
+
+func main() {
+	fmt.Println(m)
+}
