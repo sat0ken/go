ce6e8c9d34bea3a437801110fab26f25e90fab36
sat0ken <15720506+sat0ken@users.noreply.github.com>
2021-07-21 21:12:29 +0900

    add files

---
+++ gopl.io/ch2/tempconv/tempconv.go
@@ -0,0 +1,25 @@
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+const (
+    AbsoluteZeroC Celsius = -273.15
+    Freezingc Celsius = 0
+    Boiling Celsius = 100
+)
+
+func (c Celsius) String() string {
+    return fmt.Sprintf("%gC", c)
+}
+
+func (f Fahrenheit) String() string {
+    return fmt.Sprintf("%gF", f)
+}
+
+func (k Kelvin) String() string {
+    return fmt.Sprintf("%gK", k)
+}
